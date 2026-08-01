7474c58e07f92f75bcbfa872767a7f1c37c8038f
DrKariJaask <kari@lionrep.com>
2026-08-01 15:13:00 +0300

    Update site URL and add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://howtowriteyourmemoir.pages.dev"
+  site: "https://howtowriteyourmemoir.com",
+  integrations: [sitemap()],
 });
